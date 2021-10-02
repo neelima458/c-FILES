44227f4b0fe616f7050af2b275ca3184f72b8e34
Neelima Paul <72330546+neelima458@users.noreply.github.com>
2021-10-02 11:43:06 +0530

    Add files via upload

---
+++ cc.cpp
@@ -0,0 +1,22 @@
+//SOHAM SAMANTA CODES
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832
+#define ss ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
+int32_t main(){
+    ss;
+    int t;
+    cin>>t;
+    while(t--){
+    ll p;
+    int x;
+    cin>>p;
+    p%2==p%x;
+    cout<<"2"<<" "<<x<<endl;
+    }
+    return 0;
+}
